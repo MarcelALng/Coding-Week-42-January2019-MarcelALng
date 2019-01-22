65ee1844e823099c2f7ae1f59d1dc1536a87c582
Marcel AL NG <marcel.nguyen@gmail.com>
2019-01-22 22:32:28 +0100

    rb03 ex01 table_mult

--- rb03/ex00/to25.rb
+++ rb03/ex00/to25.rb
@@ -6,7 +6,7 @@ if number > 25
 	puts "erreur"
 else
 
-	for i in 1..number;
+	for i in 1..number; # pour compter et incrémenter de 0 au nombre rentré
 		puts " Dans la boucle ma variable vaut #{i} " ;
 	end
 end
---
+++ rb03/ex01/table_mult
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+puts "Entrez un nombre ";
+number = gets.to_i ;
+
+puts "la table de multiplication de #{number} est:";
+
+for i in 0..10;
+	puts "#{i}* #{number} = #{number * i}"
+end
